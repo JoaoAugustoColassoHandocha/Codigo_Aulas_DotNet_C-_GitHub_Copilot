34ae25e8a7a39e96a74b0ffc5767e50a57924933
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-27 15:39:42 -0300

    Update

--- Codigo_01/obj/Debug/net10.0/Codigo_01.AssemblyInfo.cs
+++ Codigo_01/obj/Debug/net10.0/Codigo_01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codigo_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3746292ff2b937bdc1c22dc43533925c48af602b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be0ad33d49e0159def989c3556fc7ec4506fc162")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codigo_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codigo_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
